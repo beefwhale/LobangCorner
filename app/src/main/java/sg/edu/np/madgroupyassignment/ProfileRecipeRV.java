1b474ccbbe4aafae1e2e790dfa97ff8a31d5d8e1
Hasanah Samri <s10222566@connect.np.edu.sg>
2022-06-17 00:13:49 +0800

    RecipeCorner 17th push -fixed recipe main issues

--- app/src/main/java/sg/edu/np/madgroupyassignment/ProfileRecipeRV.java
+++ app/src/main/java/sg/edu/np/madgroupyassignment/ProfileRecipeRV.java
@@ -39,8 +39,6 @@ public class ProfileRecipeRV extends Fragment{
             recipeCornersList.add(obj);
         }
 
-        buildRecyclerView();
-
         recipeRV = view.findViewById(R.id.idRVRecipe);
         adapter = new RecipeAdapter(recipeCornersList, c);
         LinearLayoutManager manager = new LinearLayoutManager(c);
@@ -50,28 +48,6 @@ public class ProfileRecipeRV extends Fragment{
         recipeRV.setAdapter(adapter);
 
         return view;
-    }
-
-    private void buildRecyclerView() {
-        //Populate user's recipe list, temporary and will change to get data from firebase
-        if (recipeCornersList.isEmpty()){
-            recipeCornersList.add(new RecipeCorner("", "4v8zoBfuGGPNY62hCSMT9UpyBAK2", "Cream Cheese Rangoons", "Crispy Cream-Cheese filled Rangoons that are easy to make at home",
-                    3, "Yongqing", "100", "step 1: ", "chikan", 1655028155253L,""));
-            recipeCornersList.add(new RecipeCorner("","4v8zoBfuGGPNY62hCSMT9UpyBAK2", "Ondeh-Ondeh", "Traditional Gula-Melaka Filled Ondeh-Ondeh",
-                    3, "Celsius", "30", "step 1: ", "chikan", 1655028155253L,""));
-            recipeCornersList.add(new RecipeCorner("","4v8zoBfuGGPNY62hCSMT9UpyBAK2", "Dumpling", "Delicious and Juicy Pork and Chives Dumpling",
-                    2, "Yong Chuen", "70", "step 1: ", "chikan", 1655028155253L,""));
-            recipeCornersList.add(new RecipeCorner("","4v8zoBfuGGPNY62hCSMT9UpyBAK2", "Pineapple Tarts", "Best-Ever Pineapple Tarts",
-                    4, "Zi Xian", "50", "step 1: ", "chikan", 1655028155253L,""));
-            recipeCornersList.add(new RecipeCorner("","4v8zoBfuGGPNY62hCSMT9UpyBAK2", "Fried Crab Sticks", "Crispy and Tasty Fried Crab Sticks",
-                    5, "Hasanah", "25",  "step 1: ", "chikan", 1655028187483L,""));
-            recipeCornersList.add(new RecipeCorner("","4v8zoBfuGGPNY62hCSMT9UpyBAK2", "Nine Layered Kueh", "Bright and Colourful Nine Layered Kueh",
-                    3, "Wesley", "45", "step 1: ", "chikan", 1655028187483L,""));
-            recipeCornersList.add(new RecipeCorner("","4v8zoBfuGGPNY62hCSMT9UpyBAK2", "Curry Path", "Chinese Curry Path",
-                    0,"Teo", "20", "step 1: ", "chikan", 1655028187483L,""));
-            recipeCornersList.add(new RecipeCorner("","4v8zoBfuGGPNY62hCSMT9UpyBAK2", "Chiffon Cake", "Pandan Chiffon Cake",
-                    4,"Q", "30", "step 1: ", "chikan", 1655028213219L,""));
-        }
     }
 
 }
