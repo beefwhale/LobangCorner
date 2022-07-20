397d2e1f753f4d8dcca50834d75c19c9261caa66
OwO11 <celsius_chia_zhan_yao@connect.ite.edu.sg>
2022-07-21 05:14:39 +0800

    -Fixed error (missing tokens) -Added nice nice

--- app/src/main/java/sg/edu/np/madgroupyassignment/FirebaseMessagingService.java
+++ app/src/main/java/sg/edu/np/madgroupyassignment/FirebaseMessagingService.java
@@ -6,6 +6,8 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.text.format.DateUtils;
+import android.widget.RemoteViews;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationManagerCompat;
@@ -21,6 +23,7 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
     private FirebaseAuth mAuth;
     private FirebaseDatabase firebaseDatabase;
     private DatabaseReference databaseReference;
+    private int id = 1;
 
     @SuppressLint("NewApi")
     @Override
@@ -35,17 +38,25 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
                 NotificationManager.IMPORTANCE_HIGH);
         getSystemService(NotificationManager.class).createNotificationChannel(channel);
 
+        RemoteViews collapsedView = new RemoteViews(getPackageName(), R.layout.notification_layout);
+        collapsedView.setImageViewResource(R.id.PushNotification_Img, R.drawable.fush);
+        collapsedView.setTextViewText(R.id.PushNotification_Title, title);
+        collapsedView.setTextViewText(R.id.PushNotification_Body, text);
+        collapsedView.setTextViewText(R.id.PushNotification_Timestamp, DateUtils.formatDateTime(this, System.currentTimeMillis(), DateUtils.FORMAT_SHOW_TIME));
+
         Intent activityIntent = new Intent(this, MainActivity.class);
         PendingIntent contentIntent = PendingIntent.getActivity(this, 0, activityIntent, 0);
 
         Notification.Builder notification = new Notification.Builder(this, CHANNEL_ID)
                 .setContentTitle(title)
                 .setContentText(text)
+                .setSmallIcon(R.drawable.fush)
                 .setContentIntent(contentIntent)
-                .setSmallIcon(R.drawable.splashpagelogo)
-                .setColor(ContextCompat.getColor(this, R.color.Main_Primary))
+                .setCustomContentView(collapsedView)
+                .setStyle(new Notification.DecoratedCustomViewStyle())
                 .setAutoCancel(true);
-        NotificationManagerCompat.from(this).notify(1, notification.build());
+        NotificationManagerCompat.from(this).notify(id, notification.build());
+        id += 1;
 
         super.onMessageReceived(message);
     }
